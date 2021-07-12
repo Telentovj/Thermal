8af6ee3a805a0771867f31ac5c5ac4a7183f953f
telentovj <johnvijaybalasupramaniam@gmail.com>
2021-07-12 16:20:23 +0800

    completed logging and anomalous video logic

--- helpers.py
+++ helpers.py
@@ -1,104 +1,137 @@
 import json
 import requests
-
+import logging
+from datetime import datetime
 def getColours(frame, bedbbox):
-    buffer = []
-    for i in range(len(bedbbox)):
-        buffer.append([])
-    
-    # j is x , k is y
-    # frmae[y][x]
-    for i in range(len(bedbbox)):
-        count = 0
-        for j in range(bedbbox[i][0], bedbbox[i][2]):
-            for k in range(bedbbox[i][1], bedbbox[i][3]):
-                count+=1
-                buffer[i].append(frame[k][j])
-        
-    return buffer
+    try:
+        buffer = []
+        for i in range(len(bedbbox)):
+            buffer.append([])
+        # j is x , k is y
+        # frmae[y][x]
+        for i in range(len(bedbbox)):
+            count = 0
+            for j in range(bedbbox[i][0], bedbbox[i][2]):
+                for k in range(bedbbox[i][1], bedbbox[i][3]):
+                    count+=1
+                    buffer[i].append(frame[k][j])
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Colour Conversion Completed")
+        return buffer
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " Colour Conversion Failed")
 
 def checkSleep(bedbboxColours,bedCounter,bedAnomallyCounter,colourThreshold,percentageThreshold,percentageAnomallyThreshold,bedTime):
-    for i in range(len(bedbboxColours)):
-        detectionCount = 0
-        countNeeded = percentageThreshold*len(bedbboxColours[i])
-        anomallyCountNeeded = percentageAnomallyThreshold*len(bedbboxColours[i])
-        for j in range(len(bedbboxColours[i])):
-            if bedbboxColours[i][j] > colourThreshold:
-                detectionCount += 1
-        if(detectionCount > countNeeded):
-            bedCounter[i] = 1
-            bedTime[i] += 0.1136
-            if(detectionCount > anomallyCountNeeded):
-                bedAnomallyCounter[i] = 1
+    try:
+        for i in range(len(bedbboxColours)):
+            detectionCount = 0
+            countNeeded = percentageThreshold*len(bedbboxColours[i])
+            anomallyCountNeeded = percentageAnomallyThreshold*len(bedbboxColours[i])
+            for j in range(len(bedbboxColours[i])):
+                if bedbboxColours[i][j] > colourThreshold:
+                    detectionCount += 1
+            if(detectionCount > countNeeded):
+                bedCounter[i] = 1
+                bedTime[i] += 1/8.8
+                if(detectionCount > anomallyCountNeeded):
+                    bedAnomallyCounter[i] = 1
+                else:
+                    bedAnomallyCounter[i] = 0
             else:
                 bedAnomallyCounter[i] = 0
-        else:
-            bedAnomallyCounter[i] = 0
-            bedCounter[i] = 0
+                bedCounter[i] = 0
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Sleep Check Completed")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " Sleep Check Failed")
+
         
 
-def updateAnomalousBehaviour(video):
-    url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/auth/login"
-    payload = json.dumps({
-        "email": "sarthak.ganoorkar@vulcan-ai.com",
-        "password": "VisionAI@052020"
-    })
-    headers = {'Content-Type': 'application/json'}
+def updateAnomalousBehaviour(videoName):
+    try:
+        url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/auth/login"
+        payload = json.dumps({
+            "email": "sarthak.ganoorkar@vulcan-ai.com",
+            "password": "VisionAI@052020"
+        })
+        headers = {'Content-Type': 'application/json'}
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    accessToken = response.json().get('accessToken')
-    print(accessToken)
+        response = requests.request("POST", url, headers=headers, data=payload)
+        accessToken = response.json().get('accessToken')
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Getting Auth Token Success")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " Getting Auth Token Failure")
 
-    #Upload fall detection
-    #status_id: 1-Aggression, 2-Fall, 3-Anomalous Behaviour, 4-Erratic Behaviour
-    #Detected by device id: 2-Camera, 3-Sensor
     
-    url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/incident"
-    payload={'incident_type_id': '2',
-    'incident_dt': '2021-07-09 10:25:14.457',
-    'status_id': '1',
-    'detected_by_device_id': '3', #2 is camera, 3 is sensor
-    'sensor_id': '3', #based on sensor in the dashboard
-    'media_type': 'video',
-    'location': 'Workplace'}
-    files=[
-        ('file',('snippet.mp4',open('/C:/Users/sarth/Vulcan/SmartCaire/Docs/snippet.mp4','rb'),'application/octet-stream'))
-    ]
-    headers = {
-        'Authorization': 'Bearer {}'.format(accessToken),
-        'Content-Type': 'application/json',
-    }
-    headers = {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhcnRoYWsuZ2Fub29ya2FyQHZ1bGNhbi1haS5jb20iLCJ1c2VyX2lkIjoiRDQyOUY3OTQtQzg4OS00RkMyLTg0MzktMDBBQkQ5QkM1RjNDIiwiY2xpZW50X2lkIjoiRTQzMzNDREItMDgzRi00NDYyLTgxNkItNjIwMTkxMDNEMTg5Iiwicm9sZSI6IkFkbWluIiwiZmlyc3RfbmFtZSI6IlNhcnRoYWsiLCJsYXN0X25hbWUiOiJHYW5vb3JrYXIiLCJyb2xlX2lkIjozLCJtYWNfaWQiOm51bGwsIndlYXJhYmxlX2lkIjpudWxsLCJ3ZWFyYWJsZV9uYW1lIjpudWxsLCJ3ZWFyYWJsZV9tb2RlbF9pZCI6bnVsbCwid2VhcmFibGVfbW9kZWxfbmFtZSI6bnVsbCwiaWF0IjoxNjI1Nzk3MTIxLCJleHAiOjE2MjU4ODM1MjF9.iptgJFHfkeUnEMZ4mfg3kB2mkDlRhO-XKKZcMTXQh5o'
-    }
+    try:
+        #Upload fall detection
+        #status_id: 1-Aggression, 2-Fall, 3-Anomalous Behaviour, 4-Erratic Behaviour
+        #Detected by device id: 2-Camera, 3-Sensor
+        
+        url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/incident"
+        payload={'incident_type_id': '2',
+        'incident_dt': '2021-07-09 10:25:14.457',
+        'status_id': '1',
+        'detected_by_device_id': '3', #2 is camera, 3 is sensor
+        'sensor_id': '3', #based on sensor in the dashboard
+        'media_type': 'video',
+        'location': 'Workplace'}
+        files=[
+            ('file',(videoName + '.avi',open('/home/vulcan/Documents/Thermal/AnomallyVideo/' + videoName + '.avi','rb'),'application/octet-stream'))
+        ]
+        headers = {
+            'Authorization': 'Bearer {}'.format(accessToken),
+            'Content-Type': 'application/json',
+        }
 
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+        response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
-    print(response.text)
+        print(response.text)
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Anomalous Detection Video Upload Completed")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " Anomalous Detection Video Upload Failed")
+
 
 def updateBedTime(bedTime):
-    url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/auth/login"
-    payload = json.dumps({
-        "email": "sarthak.ganoorkar@vulcan-ai.com",
-        "password": "VisionAI@052020"
-    })
-    headers = {'Content-Type': 'application/json'}
+    try:
+        url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/auth/login"
+        payload = json.dumps({
+            "email": "sarthak.ganoorkar@vulcan-ai.com",
+            "password": "VisionAI@052020"
+        })
+        headers = {'Content-Type': 'application/json'}
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    accessToken = response.json().get('accessToken')
-    print(accessToken)
+        response = requests.request("POST", url, headers=headers, data=payload)
+        accessToken = response.json().get('accessToken')
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Getting Auth Token Success")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " Getting Auth Token Failure")
+    
+    try:
+        # Save the time spend in bed
+        url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/user/time-in-bed"
+        payload = json.dumps({
+            "user_id": "d429f794-c889-4fc2-8439-00abd9bc5f3c",
+            "datetime": "2021-07-09",
+            "time_spent": bedTime  # hours/minutes?
+        })
+            
+        headers = {'Authorization': 'Bearer {}'.format(accessToken),
+                    'Content-Type': 'application/json',
+                }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text)
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " BedTime Upload Completed")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.error(current + " BedTime Upload Failed")
 
-    # Save the time spend in bed
-    url = "https://smartcaire-dev.azurewebsites.net/api/v2.0/user/time-in-bed"
-    payload = json.dumps({
-        "user_id": "d429f794-c889-4fc2-8439-00abd9bc5f3c",
-        "datetime": "2021-07-09",
-        "time_spent": 6656  # hours/minutes?
-    })
-        
-    headers = {'Authorization': 'Bearer {}'.format(accessToken),
-                'Content-Type': 'application/json',
-            }
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    # Save video of anomalous behaviour
--- main.py
+++ main.py
@@ -2,37 +2,68 @@
 # py main.py --video video.avi
 
 # import the necessary packages
+from datetime import datetime
 from imutils.video import FPS
 import argparse
 import cv2
+import logging
 from helpers import *
 
+# import logging
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+# logging.debug('This message should go to the log file')
+# logging.info('So should this')
+# logging.warning('And this, too')
+# logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
 def startStream(args):
+    #Logger
+    current = datetime.now().strftime('%Y-%m-%d')
+    logging.basicConfig(filename= "/home/vulcan/Documents/Thermal/Log/" + current + ".log", level=logging.INFO)
 
-    frameQueue = []
-    # initialize list of beds, each bed will be a series of bounding boxes.
-    # xmin ymin xmax ymax
-    bedbbox=[[50,90,75,118]]
-    # bedbbox=[[10,80,35,118],[50,80,75,118],[90,80,115,118]]
-    #0-255
-    colourThreshold = 130
-    percentageThreshold = 0.3
-    percentageAnomallyThreshold = 0.5
-    bedCounter = [0]*len(bedbbox)
-    bedAnomallyCounter = [0]*len(bedbbox)
-    bedTime = [0]*len(bedbbox)
-    
-    # to track the frames
-    totalFrames = 0
+    try:
+        #Track how many more frames to capture, if at the end of the required frames, we still have an 
+        #anomally detection then we extend the frames required until we reach a point where frame 
+        #required = 0 and no more anomally detections are found.
+        anomallyFrameTracker = [0]
+        accumulativeFrameQueue = []
+        movingFrameQueue = []
 
 
-    # initialize the video stream and output video writer
-    print("[INFO] starting video stream...")
-    vs = cv2.VideoCapture(args['video'])
-    writer = None
+        # initialize list of beds, each bed will be a series of bounding boxes.
+        # xmin ymin xmax ymax
+        bedbbox=[[50,90,75,118]]
+        # bedbbox=[[10,80,35,118],[50,80,75,118],[90,80,115,118]]
 
-    # start the frames per second throughput estimator
-    fps = FPS().start()
+        #0-255
+        colourThreshold = 130
+        #Percentage of bed bbox needed to be covered by pixels with colour > 130 to be considered detection
+        percentageThreshold = 0.3
+        #Same as above but stricter for anomally detection
+        percentageAnomallyThreshold = 0.5
+        #Initialize the counter that will keep track of number of people on each bed.
+        bedCounter = [0]*len(bedbbox)
+        #Initialize the counter that will keep track of number of anomalous behaviour on each bed.
+        bedAnomallyCounter = [0]*len(bedbbox)
+        #Initialize the counter that will keep track of time spend on each bed.
+        bedTime = [0]*len(bedbbox)
+        
+        # to track the frames
+        totalFrames = 0
+
+
+        # initialize the video stream and output video writer
+        print("[INFO] starting video stream...")
+        vs = cv2.VideoCapture(args['video'])
+        writer = None
+
+        # start the frames per second throughput estimator
+        fps = FPS().start()
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Parameter Generation Success")
+    except:
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Parameter Generation Failed")
 
 
     # loop over frames from the video file stream
@@ -41,6 +72,8 @@ def startStream(args):
         (grabbed, frame) = vs.read()
         # check to see if we have reached the end of the video file
         if frame is None:
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " Video ended")
             break
         else:
             # resize the frame for faster processing and then convert the
@@ -50,14 +83,16 @@ def startStream(args):
             checkSleep(bedbboxColours,bedCounter,bedAnomallyCounter,colourThreshold,percentageThreshold,percentageAnomallyThreshold,bedTime)
             
             frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
- 
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " Frame manipulations completed")
             # if we are supposed to be writing a video to disk, initialize
             # the writer
         if args["output"] is not None and writer is None:
             fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-            writer = cv2.VideoWriter(args["output"], fourcc, 10,
+            writer = cv2.VideoWriter(args["output"], fourcc, 60,
                 (frame.shape[1], frame.shape[0]), True)
-
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " Writer Initialized")
 
         for i in range(len(bedbbox)):
             if bedAnomallyCounter[i] > 0:
@@ -68,7 +103,8 @@ def startStream(args):
                 cv2.putText(frame,  str(round(bedTime[i],1)), (bedbbox[i][0], bedbbox[i][1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.20, (0,0,255), 1) 
             else:
                 cv2.putText(frame,  str(round(bedTime[i],1)), (bedbbox[i][0], bedbbox[i][1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.20, (200,120,200), 1) 
-               
+        current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+        logging.info(current + " Bounding Box Drawn")
 
         if writer is not None:
             writer.write(frame)
@@ -85,25 +121,84 @@ def startStream(args):
         # update the FPS counter
         fps.update()
 
-        if len(frameQueue) > 200:
-            frameQueue.pop()
+
+        # Call for frame requirements if there is an anomally
+        if len(movingFrameQueue) > 200:
+            movingFrameQueue.pop(0)
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " MovingFrameQueue Popped")
         else:
-            frameQueue.append(frame)
+            movingFrameQueue.append(frame)
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " MovingFrameQueue Appended")
         
-        if bedAnomallyCounter[i] > 0:
-            print('Save Video')
-            #updateAnomalousBehaviour(video)
-        #Save video code
+
+
+        # Add frame requirements to track anomally
+        flag = False
+        for i in range(len(bedbbox)):
+            if bedAnomallyCounter[i] > 0:
+                flag = True
+                current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+                logging.info(current + " Anomally Detected, starting call for more frames")
+
+        if flag:
+            if anomallyFrameTracker[0] == 0:
+                for frame in movingFrameQueue:
+                    accumulativeFrameQueue.append(frame)
+            anomallyFrameTracker[0] = 200
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " anomallyFrameTracker resetted to 200")
+
+        # If we find a call for frame requirements, we append the frame then reduce the frame requirements incrementally
+        if anomallyFrameTracker[0] != 0:
+            accumulativeFrameQueue.append(frame)
+            anomallyFrameTracker[0] -= 1
+
+        try:
+            if anomallyFrameTracker[0] == 0 and len(accumulativeFrameQueue) >= 400:
+                anomallyfourcc = cv2.VideoWriter_fourcc(*"MJPG")
+                current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+                anomallyWriter = cv2.VideoWriter('/home/vulcan/Documents/Thermal/AnomallyVideo/' + current + ".avi", anomallyfourcc , 60,
+                    (frame.shape[1], frame.shape[0]), True)
+                for pic in  accumulativeFrameQueue:
+                    anomallyWriter.write(pic)
+                anomallyWriter.release()
+                accumulativeFrameQueue = []
+                updateAnomalousBehaviour(current)
+        except:
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " Anomally Video Output unsuccessful")
+
 
     # stop the timer and display FPS information
     fps.stop()
     print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
     print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+    logging.info(current + "[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    logging.info(current + "[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
     # check to see if we need to release the video writer pointer
     if writer is not None:
         writer.release()
-    # updateBedTime(bedTime)
+    if len(accumulativeFrameQueue) != 0:
+        try:
+            anomallyfourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            anomallyWriter = cv2.VideoWriter( '/home/vulcan/Documents/Thermal/AnomallyVideos/' + current + ".avi", anomallyfourcc , 60,
+                (frame.shape[1], frame.shape[0]), True)
+            for pic in  accumulativeFrameQueue:
+                anomallyWriter.write(pic)
+            anomallyWriter.release()
+            accumulativeFrameQueue = []
+            updateAnomalousBehaviour(current)
+        except:
+            current = datetime.now().strftime('%Y-%m-%d-%H--%M--%S')
+            logging.info(current + " Anomally Video Output unsuccessful")
+    
+
+    updateBedTime(bedTime)
     # do a bit of cleanup
     cv2.destroyAllWindows()
     vs.release()
